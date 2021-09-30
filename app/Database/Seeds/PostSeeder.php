25350a17087459df981c0ae8fb74a6a3aad85e92
vadella1054 <vadellanikitaayumi@gmail.com>
2021-09-30 15:18:03 +0700

    third commit

---
+++ app/Database/Seeds/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+	public function run()
+	{
+			$model = model('PostModel');
+
+			$kategori =['nature', 'programming', 'cat', 'sport', 'lifestyle', 'food'];
+
+			for($i = 0; $i < 6; $i++){
+			$model->insert([
+					'judul'      => static::faker()->sentence(3),
+					'slug' 		 => static::faker()->unique()->slug(3),
+					'author' 	 => static::faker()->name(),
+					'gambar' 	 => $kategori[0],
+					'deskripsi'  => static::faker()->text(),
+			]);
+	}
+}
+}
